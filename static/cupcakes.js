1342370ddc798c7b3727b5af997cf197b6041465
Alejandro Sacripanti <asacripanti93@gmail.com>
2023-11-09 11:25:55 -0600

    styles

--- static/cupcakes.js
+++ static/cupcakes.js
@@ -1,18 +1,38 @@
 const BASE_URL = "http://127.0.0.1:5000/api";
 
+const sizeContainer = document.querySelector(".sizeContainer")
+
+
 function createCupcakeHTML(cupcake){
     return `
-    <div data-cupcake-id=${cupcake.id}>
-      <li>
-        ${cupcake.flavor} / ${cupcake.size} / ${cupcake.rating}
-        <button class="delete-button">X</button>
-      </li>
-      <img class="Cupcake-img"
-            src="${cupcake.image}"
-            alt="(no image provided)">
+    
+    <div class="cupcakeShell" data-cupcake-id=${cupcake.id}>
+      <button class="delete-button">X</button>
+      <h2 class="cupcakeFlavor">${cupcake.flavor}</h2>
+      <img class="cupcakeImg"src="${cupcake.image}"alt="(no image provided)">
+      <section class="rating_sizeShell">
+        <div class="ratingContainer">
+          <span class="cupcakeRating">${cupcake.rating}</span>
+        </div>
+        <div class="sizeContainer">
+          ${cupcake.size}    
+        </div> 
+      </section>
     </div>
   `;
 }
+
+function appendCupcakeSizeImg(cupcakeData){
+  let newImg = document.createElement('img');
+
+  if(cupcakeData.size === 'large'){
+    newImg.src = "https://sallysbakingaddiction.com/wp-content/uploads/2017/06/moist-chocolate-cupcakes-5.jpg"
+  }
+
+  sizeContainer.appendChild(newImg);
+
+}
+
 
 async function showInitialCupcakes() {
     const response = await axios.get(`${BASE_URL}/cupcakes`);
@@ -20,6 +40,8 @@ async function showInitialCupcakes() {
     for (let cupcakeData of response.data.cupcakes) {
       let newCupcake = $(createCupcakeHTML(cupcakeData));
       $("#cupcakesList").append(newCupcake);
+
+      // appendCupcakeSizeImg(cupcakeData);
     }
   }
 
